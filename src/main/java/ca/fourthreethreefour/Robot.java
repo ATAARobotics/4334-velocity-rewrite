92f56915a540d9a4893dfeb2ea7ea3dd48bedeb7
Kulkinz <cool@coolspages.com>
2019-01-02 22:21:50 -0700

    Added Drive Code

--- src/main/java/ca/fourthreethreefour/Robot.java
+++ src/main/java/ca/fourthreethreefour/Robot.java
@@ -1,8 +1,10 @@
 package ca.fourthreethreefour;
 
 import edu.first.module.Module;
+import edu.first.module.joysticks.XboxController;
 import edu.first.module.subsystems.Subsystem;
 import edu.first.robot.IterativeRobotAdapter;
+import edu.first.module.joysticks.BindingJoystick.DualAxisBind;
 
 public class Robot extends IterativeRobotAdapter implements Constants {
 
@@ -26,6 +28,19 @@ public class Robot extends IterativeRobotAdapter implements Constants {
     public void init() {
         // Initializes all modules
         ALL_MODULES.init();
+
+        controller.addDeadband(XboxController.LEFT_FROM_MIDDLE, 0.12);
+        controller.changeAxis(XboxController.LEFT_FROM_MIDDLE, speedFunction);
+        controller.addDeadband(XboxController.RIGHT_X, 0.12);
+        controller.invertAxis(XboxController.RIGHT_X);
+        controller.changeAxis(XboxController.RIGHT_X, turnFunction);
+
+        controller.addAxisBind(new DualAxisBind(controller.getLeftDistanceFromMiddle(), controller.getRightX()) {
+			@Override
+			public void doBind(double speed, double turn) {
+                drivetrain.arcadeDrive(speed, turn);
+            }
+        });
     }
 
     @Override
